9379a818f3c4335a284baf6c766e766feaebf718
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:40:34 -0500

    Add files via upload

---
+++ practica 3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   //double numero = 100.5;
+   //printf("El contenido de la variable es %cd", caracter);
+
+   int n;
+   printf(" Introduce un numero \n");
+  scanf("%d",&n);
+   printf("El numero introduccion es: %d", n);
+}
