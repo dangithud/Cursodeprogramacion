763e12b0e58bd9938a7f548aa562d0711e11dc71
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 14:51:10 -0500

    Add files via upload

---
+++ Tipos enumerados/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    typedef enum {lunes,martes,miercoles,jueves,viernes,sabado,domingo} semana;
+
+    semana dia_semana1 = lunes;
+
+    if (dia_semana1 = 0){
+            printf("Estamos en lunes\n");
+    }
+    else{
+            printf("No estamos en lunes\n");
+    }
+
+}
