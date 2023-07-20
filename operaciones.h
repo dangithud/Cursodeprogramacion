4b2be5f4825e5d60b97d10d680d0a5a9a7384499
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 14:53:09 -0500

    Add files via upload

---
+++ operaciones.h
@@ -0,0 +1,9 @@
+#ifndef OPERACIONES_H_INCLUDED
+#define OPERACIONES_H_INCLUDED
+int sumar(int op1, int op2);
+int restar(int op1, int op2);
+int multiplicar(int op1, int op2);
+float dividir(int op1, int op2);
+
+
+#endif // OPERACIONES_H_INCLUDED
