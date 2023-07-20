ff5a42b84e925c0beafde109220598e51da7199a
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:35:31 -0500

    Add files via upload

---
+++ Estructuras de control alternativas  if/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero = 3;
+    int numero2 = 7;
+    int numero3 = 1;
+
+    if (numero > 0 && numero2 > 0){
+        if (nuermo3> 0){
+            printf("Los tres numeros en positivos\n");
+        }
+
+
+    }
+    else if(numero >0  || numero2 > 0){
+        printf("Alguno de los numeros es positivo\n");
+    }
+    else{
+        printf("Ningun de los dos son positivos\n");
+    }
+
+    printf("Fin del programa\n");
+}
