9d327d8f67669e537da08a0330b3a777c8b3a232
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:35:55 -0500

    Add files via upload

---
+++ Estructuras de control alternativas switch/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n = 2;
+
+    switch(n>0){
+    case(1):{
+        printf("N es mayor de 0");
+        break;
+    }
+    case(0):{
+        printf("N no es mayor de 0");
+        break;
+
+    }
+    }
+}
