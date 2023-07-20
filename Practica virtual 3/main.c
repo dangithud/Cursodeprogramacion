a44736f541f161c1ba48eacde915becfb49fe0e5
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:41:38 -0500

    Add files via upload

---
+++ Practica virtual 3/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float euros;
+   printf("Introduce los euros que quieres convertir\n");
+   scanf("%f",&euros);
+   //pesetas = euros * 166.386
+   printf("%.2f euros equivalen a %.2f pesetas",euros*166.386);
+}
