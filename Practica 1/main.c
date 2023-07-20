95808c94b6879a7c126d2b3f72660265521d0386
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:40:03 -0500

    Add files via upload

---
+++ Practica 1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Este programa ejemplifica el uso del entorno de desarollo para escribir programas en lenguaje C,
+ mediante una instruccion que muestra mensaje de bienvenido por la persona*/
+int main()
+{
+    //Muestra por pantalla el mensaje indicado
+    printf("Hola mundo Lenguaje C\n");
+    return 0;
+}
