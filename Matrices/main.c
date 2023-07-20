8b285ca6af3a993e98d886a1ed27da1bc238e51b
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:39:29 -0500

    Add files via upload

---
+++ Matrices/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int m[3][2] = {{0,1},{2,3},{4,5}};
+
+    //printf("%d\n",m[1][1]);
+
+    m[2][0] = 40;
+
+    for (int i=0; i<3; i++){
+        for(int j=0; j<2; j++){
+            printf("%d\n",m[i][j]);
+        }
+        printf("\n");
+    }
+}
