9892b815834814fe0a8b8316ae0e2eedbdba3946
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 14:47:30 -0500

    Add files via upload

---
+++ Practica virtual 16/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    struct hora_entrada{
+        int hora;
+        int minuto;
+    };
+
+    struct entrada{
+        struct hora_entrada hora1;
+        int asistentes;
+        float precio;
+    };
+
+    struct entrada e1;
+
+    char continuar = 's';
+
+    while (continuar == 's'){
+
+        printf("Introduce la hora de entrada\n");
+        scanf("%d",&e1.hora1.hora);
+        printf("Introduce el minuto de entrada\n");
+        scanf("%d",&e1.hora1.minuto);
+        printf("Introduce el numero de asistentes\n");
+        scanf("%d",&el.asistentes);
+
+        int total = 0;
+
+        for (int i=0; i<el.asistentes; i++){
+
+            int edad;
+            printf("Introduce la edad del asistente %d", (i+1));
+            scanf("%d",&edad);
+            if (edad < 6){
+                total = total + 0;
+            }
+            else if(edad >=6 && edad <=15){
+                total = total + 5;
+            }
+            else if(edad >=16 && edad <= 26)  || edad > 65){
+                total = total + 8;
+            }
+            else{
+                total = total + 10;
+        }
+        if (e1.asistentes >= 5){
+            e1.precio = total;
+        }
+        printf("Quiere continuar con otro grupo?(S/N)\n");
+        scanf("%c",&continuar);
+
+    }
+
+    printf("Hasta la proxima...\n");
+}
