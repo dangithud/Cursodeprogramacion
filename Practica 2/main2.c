e64ba316797c4ecab8977474ff1bed33aaf5329b
dangithud <130512101+dangithud@users.noreply.github.com>
2023-07-20 13:40:17 -0500

    Add files via upload

---
+++ Practica 2/main2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  //int: 8, -15, 1987
+  //float: 5.34, 8.57, -100,5
+  //double: 1000.653, -200.65
+  //char: "t", "a","9","/n"
+
+   int numero_entero = 16;
+   float numero_real_simple = 5.6;
+   double numero_real_doble = 10000.567;
+   char caracter = "t";
+
+}
